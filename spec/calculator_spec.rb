7767e58e39299d7a90537623fb6b225ee34c45f2
Ingram Monk <ingram.monk@gmail.com>
2014-07-01 07:03:19 +0100

    Created string calculator and accept 0 or 1 argument

---
+++ spec/calculator_spec.rb
@@ -0,0 +1,46 @@
+# String Calculator
+
+# Create a simple String calculator with a method int Add(string numbers)
+# The method can take 0, 1 or 2 numbers, and will return their sum (for an empty string it will return 0) for example “” or “1” or “1,2”
+
+
+#require 'calculator'
+
+class Calculator
+
+  def initialize
+
+  end
+
+  def Add arg=0
+    if arg == 0
+      0
+    else
+      1
+    end
+  end
+
+  def to_s
+    true
+  end
+
+end
+
+describe Calculator do
+
+  it "should instantiate and return true on string" do
+    # @calculator =
+    expect(Calculator.new().to_s).to eq(true)
+  end
+
+  it "should return an integer when Adding a string" do
+    @calculator = Calculator.new
+    expect(@calculator.Add("qwerty")).to eq(1)
+  end
+
+  it "should accept 0 numbers and return 0" do
+    @calculator = Calculator.new
+    expect(@calculator.Add()).to eq(0)
+  end
+
+end
