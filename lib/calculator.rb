bcd89d59a401a7a12b2d494b8abecd464095ffa6
Dan Sharp <drsharp@sharpdesigns.com>
2014-07-09 17:35:48 -0600

    Update calculator.rb
    Clean up method to be more in line with traditional Ruby styling.

--- lib/calculator.rb
+++ lib/calculator.rb
@@ -1,24 +1,10 @@
 
 class Calculator
 
-  def initialize
-
-  end
-
-  def Add arg="0"
-
-    arg.gsub! '\n', ','
+  def add(arg='0')
+    arg.gsub!('\n', ',')
     args = arg.split(",")
-
-    sum = 0
-
-    args.each { |x| sum += x.to_i()}
-
-    sum
-  end
-
-  def to_s
-    true
+    args.map(&:to_i).reduce(:+)
   end
 
 end
